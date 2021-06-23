25809f531e7f4a89c4501f52acf010c2faf81ab8
Vilma Qerama <vilma.qerama@hotmail.com>
2021-06-24 00:26:06 +0100

    try with updated env options

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,9 +1,9 @@
 const sequelize = require("sequelize");
 
 const dbOptions = {
-  host: process.env.DB_HOST,
+  host: "	esilxl0nthgloe1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
   dialect: "mysql",
-  port: process.env.DB_PORT,
+  port: 3306,
   logging: false,
 };
 
@@ -11,10 +11,14 @@ const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+const dbNameH = "tunzn9nuuemxrzcj";
+const dbUserH = "cwe5by7jy7qwxiwy";
+const dbPasswordH = "gpfweonx84iqptlp";
+
 let connection;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(dbNameH, dbUserH, dbPasswordH, dbOptions);
 } else {
   // connection for local development
   connection = new sequelize(dbName, dbUser, dbPassword, dbOptions);
