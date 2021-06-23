4f082fac9d1e7c639efb36c8e8995de5669c0af3
Vilma Qerama <vilma.qerama@hotmail.com>
2021-06-23 17:47:20 +0100

    add view routes

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -1,23 +1,12 @@
 const { Router } = require("express");
 
-const renderHomePage = require("../../controllers/view/renderHomePage");
-const renderLoginPage = require("../../controllers/view/renderLoginPage");
-const renderSignupPage = require("../../controllers/view/renderSignupPage");
-const renderDashboardPage = require("../../controllers/view/renderDashboardPage");
-const renderPostPage = require("../../controllers/view/renderPostPage");
-const renderCreatePostPage = require("../../controllers/view/renderCreatePostPage");
+const auth = require("../../middleware/auth");
+const privateRouter = require("./privateRouter");
+const publicRouter = require("./publicRouter");
 
 const router = Router();
 
-router.get("/sign-up", renderSignupPage);
-router.get("/login", renderLoginPage);
-// add auth middleware here
-router.get("/dashboard", renderDashboardPage);
-// add auth middleware here
-router.get("/posts/:id", renderPostPage);
-// add auth middleware here
-router.get("/create-post", renderCreatePostPage);
-
-router.get("/", renderHomePage);
+router.use(publicRouter);
+router.use(auth, privateRouter);
 
 module.exports = router;
