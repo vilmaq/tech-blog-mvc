0ee9117c1cadaf811a45467d1e58cd54a89c3531
Vilma Qerama <vilma.qerama@hotmail.com>
2021-07-10 22:02:37 +0100

    update sequelize to Sequelize index.js

--- src/db/seed/index.js
+++ src/db/seed/index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const sequelize = require("../../config/connection");
+const Sequelize = require("../../config/connection");
 const { User, Post, Comment } = require("../../models");
 const users = require("./data/users.json");
 const posts = require("./data/posts.json");
 const comments = require("./data/comments.json");
 
 const seed = async () => {
-  await sequelize.sync({ force: true });
+  await Sequelize.sync({ force: true });
 
   await User.bulkCreate(users);
   console.log("Successfully seeded users");
